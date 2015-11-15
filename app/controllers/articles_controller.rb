df499d93801d20974aa389e00b1c34a40f9ed449
rachaellynn <rachael@wordaroundtown.com>
2015-11-15 12:11:53 -0500

    added request for authentication but not ability to login. this completes the tutorial.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+	
+#adding in authentication, using rails built-in authentication, rather than devise this time
+http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
 	def index
     @articles = Article.all
   	end
