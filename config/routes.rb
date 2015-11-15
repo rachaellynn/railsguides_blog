ba671ed970ebec29a12551385e2334cf63b6e6f5
rachaellynn <rachael@wordaroundtown.com>
2015-11-15 11:51:22 -0500

    added comments model and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :articles
+  resources :articles do
+  resources :comments
+  end
+
   # You can have the root of your site routed with "root"
   #tells Rails to map requests to the root of the application to the welcome controller's index action
   root 'welcome#index'
